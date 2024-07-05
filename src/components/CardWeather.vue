7c4778b0a453d0c0ab8013cbcbcff681c1df5620
Oleg <skorpik20@gmail.com>
2024-07-05 16:12:07 +0300

    add project

---
+++ src/components/CardWeather.vue
@@ -0,0 +1,13 @@
+<template>
+  $END$
+</template>
+
+<script>
+    export default {
+        name: "CardWeather"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/ModalAdd.vue
@@ -0,0 +1,13 @@
+<template>
+  $END$
+</template>
+
+<script>
+    export default {
+        name: "ModalAdd"
+    }
+</script>
+
+<style scoped>
+
+</style>
