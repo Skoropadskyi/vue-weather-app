25d6028d4b777e97125e1ffac3f09326baabf65b
Oleg <skorpik20@gmail.com>
2024-07-08 13:48:08 +0300

    add project

--- deploy.sh
+++ deploy.sh
@@ -13,6 +13,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push -f git@github.com:Skoropadskyi/weather-app.git master:gh-pages
+git push -f git@github.com:Skoropadskyi/vue-weather-app.git master:gh-pages
 
 cd -
